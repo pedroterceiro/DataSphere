c57379d54728b2e5c531c43d1a99672f04e4e0ed
Pedro <admin@scarlassara.com>
2026-04-10 16:02:49 -0300

    feat: add roles and policies for nodes and controllers

--- modules/security/iam/main.tf
+++ modules/security/iam/main.tf
@@ -1,5 +1,5 @@
-resource "aws_iam_role" "eks" {
-  name = "eks-pod-identity"
+resource "aws_iam_role" "eks-csi-controller" {
+  name = "eks-pod-identity-efs-csi-controller"
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
@@ -14,7 +14,28 @@ resource "aws_iam_role" "eks" {
   })
 }
 
-resource "aws_iam_role_policy_attachment" "csidriver" {
-  role = aws_iam_role.eks.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+resource "aws_iam_role" "eks-csi-node" {
+  name = "eks-pod-identity-efs-csi-node"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = ["sts:AssumeRole", "sts:TagSession"]
+        Effect = "Allow"
+        Principal = {
+          Service = "pods.eks.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachments_exclusive" "efs-csi-controller-sa" {
+  role_name = aws_iam_role.eks-csi-controller.name
+  policy_arns = ["arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy", "arn:aws:iam::aws:policy/service-role/AmazonS3FilesCSIDriverPolicy"]
+}
+
+resource "aws_iam_role_policy_attachments_exclusive" "efs-csi-node-sa" {
+  role_name = aws_iam_role.eks-csi-node.name
+  policy_arns = ["arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess", "arn:aws:iam::aws:policy/AmazonElasticFileSystemsUtils"]
 }
