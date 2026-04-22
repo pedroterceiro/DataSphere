0fe32fa7decf18a9470030c38abe00eebedb4385
Pedro <admin@scarlassara.com>
2026-04-22 13:47:32 -0300

    feat: proper AMI type

--- modules/compute/eks/main.tf
+++ modules/compute/eks/main.tf
@@ -5,28 +5,10 @@ resource "aws_eks_cluster" "this" {
   version  = "1.35"
 
   access_config {
-    authentication_mode = "API"
+    authentication_mode = "API_AND_CONFIG_MAP"
   }
 
-  compute_config {
-    enabled       = true
-    node_pools    = ["general-purpose"]
-    node_role_arn = var.cluster_role_arn
-  }
-
-  bootstrap_self_managed_addons = false
-
-  kubernetes_network_config {
-    elastic_load_balancing {
-      enabled = true
-    }
-  }
-
-  storage_config {
-    block_storage {
-      enabled = true
-    }
-  }
+  bootstrap_self_managed_addons = true
 
   vpc_config {
     endpoint_private_access = true
@@ -44,7 +26,7 @@ resource "aws_eks_node_group" "general" {
   node_group_name = "${var.project_name}-eks-node-group-general"
   node_role_arn   = var.node_group_role_arn
   subnet_ids      = var.private_subnets
-  ami_type        = "AL2_x86_64"
+  ami_type        = "AL2023_x86_64_STANDARD"
   instance_types  = ["t2.medium"]
 
   scaling_config {
@@ -59,15 +41,17 @@ resource "aws_eks_node_group" "general" {
 }
 
 resource "aws_eks_addon" "ebs_csi_driver" {
-  cluster_name = aws_eks_cluster.this.name
-  addon_name   = "aws-ebs-csi-driver"
+  cluster_name             = aws_eks_cluster.this.name
+  addon_name               = "aws-ebs-csi-driver"
+  service_account_role_arn = var.csi_controller_role_arn
 
   depends_on = [aws_eks_node_group.general]
 }
 
 resource "aws_eks_addon" "efs_csi_driver" {
-  cluster_name = aws_eks_cluster.this.name
-  addon_name   = "aws-efs-csi-driver"
+  cluster_name             = aws_eks_cluster.this.name
+  addon_name               = "aws-efs-csi-driver"
+  service_account_role_arn = var.csi_controller_role_arn
 
   depends_on = [aws_eks_node_group.general]
 }
